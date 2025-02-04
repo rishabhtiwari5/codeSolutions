9db4782809d169bb56eb598daac3acc9f4dc3a35
Rishabh Tiwari <145759085+rishabhtiwari5@users.noreply.github.com>
2025-02-04 23:10:23 +0530

    Create rotateFunction.cpp

---
+++ rotateFunction.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& A) {
+        if (A.empty()) return 0;
+        long long f = 0, ans = INT_MIN, N = A.size(), sum = accumulate(A.begin(), A.end(), (long long)0);
+        for (int i = 0; i < N; ++i) f += i * A[i];
+        for (int i = N - 1; i >= 0; --i) ans = max(ans, f += (sum - N * A[i]));
+        return ans;
+    }
+};
